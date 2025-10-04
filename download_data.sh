c86a01c3aab4640b5a843c702176a37dbf399114
yuki-2025 <159591455+yuki-2025@users.noreply.github.com>
2025-10-04 18:28:58 -0500

    Add files via upload

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+# 下载微调数据集
+modelscope download --dataset 'swift/ChartQA' --local_dir 'data/sft'
+
+# 下载mrag数据集
+git clone https://huggingface.co/datasets/gigant/pdfvqa data/mrag
+
---
+++ download_models.sh
@@ -0,0 +1,6 @@
+modelscope download --model Qwen/Qwen2.5-VL-7B-Instruct --local_dir 'models/qwen2.5vl'
+
+git clone https://huggingface.co/vidore/colqwen2-v1.0 models/colqwen2-v1.0
+git clone https://huggingface.co/vidore/colqwen2-base models/colqwen2-base
+
+git clone https://huggingface.co/lightonai/MonoQwen2-VL-v0.1 models/MonoQwen2-VL-v0.1
